a3889f49a832d5542f9e94779a7fc9e6828ecb6d
gracenephele <gracenephele@naver.com>
2022-03-17 21:53:54 +0900

    220317 중간

--- 2DMockUpPrj/CScene_Title.cpp
+++ 2DMockUpPrj/CScene_Title.cpp
@@ -1,2 +1,32 @@
 #include "framework.h"
 #include "CScene_Title.h"
+//#include "CImageObject.h"
+
+CScene_Title::CScene_Title() {
+
+}
+
+CScene_Title::~CScene_Title() {
+
+}
+void CScene_Title::Enter() {
+	//TODO:
+
+	//1.로고 출력용 오브젝트 제작
+	
+/* 
+	CImageObject* logoObject = new CImageObject;
+	
+
+	*/
+	//2. 시작 버튼
+//3. 종료 버튼
+
+}
+
+
+
+
+
+
+
--- 2DMockUpPrj/CScene_Title.h
+++ 2DMockUpPrj/CScene_Title.h
@@ -1,8 +1,15 @@
 #pragma once
-class CScene_Title
+#include "CScene.h"
+class CScene_Title : 
+    public CScene
 {
 
+public:
+    CScene_Title();
+    virtual ~CScene_Title();
 
+    virtual void Enter();
+    virtual void Exit();
 
 
 };
